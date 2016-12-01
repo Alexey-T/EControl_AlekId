b8850f89ae92a58bfb89a6d34817001a4e08cc5a
Alexey-t <support@uvviewsoft.com>
2016-12-02 00:20:53 +0300

    optimiz FoFindLexerForFilename

--- ec/proc_lexer.pas
+++ ec/proc_lexer.pas
@@ -17,75 +17,72 @@ uses
   ecStrUtils,
   ATStringProc;
 
-function DoFindLexerForFilename(LexLib: TecSyntaxManager; const FileName: string): TecSyntAnalyzer;
+function DoFindLexerForFilename(LexLib: TecSyntaxManager; FileName: string): TecSyntAnalyzer;
 function DoGetLexerFileFilter(an: TecSyntAnalyzer; const AllFilesText: string): string;
 function DoGetLexerDefaultExt(an: TecSyntAnalyzer): string;
 
 implementation
 
+function SItemListed(const AItem, AList: string): boolean;
+begin
+  //space separated list
+  if (AItem='') or (AList='') then
+    Result:= false
+  else
+    Result:= Pos(' '+AItem+' ', ' '+AList+' ' )>0;
+end;
+
 {
 This finds lexer by Extensions-property of lexer.
-It is space-separated items.
+It is space-separated items. In lower case.
 Items are
 - usual extension: "pas" finds "dir/filename.pas"
 - double extension (higher priority): "some.html" finds "dir/myfile.some.html"
   (before lexer HTML finds it)
 - filename: "/name.ext" finds "any/dir/name.ext"
 }
-function DoFindLexerForFilename(LexLib: TecSyntaxManager; const FileName: string): TecSyntAnalyzer;
+function DoFindLexerForFilename(LexLib: TecSyntaxManager; FileName: string): TecSyntAnalyzer;
 var
   fname, ext1, ext2: string;
   i: integer;
-  st: TzStringList;
 begin
   Result:= nil;
 
-  fname:= '/' + LowerCase(ExtractFileName(FileName));
+  //strip path, lower case
+  FileName:= LowerCase(ExtractFileName(FileName));
+  fname:= '/'+FileName;
 
-  //final extension
-  ext1:= LowerCase(ExtractFileExt(FileName));
+  //usual extension
+  ext1:= ExtractFileExt(FileName);
   if SBeginsWith(ext1, '.') then Delete(ext1, 1, 1);
 
-  //2nd+final extension
+  //double extension
   ext2:= '';
   if ext1<>'' then
   begin
-    ext2:= LowerCase(ExtractFileExt(ChangeFileExt(FileName, '')));
+    ext2:= ExtractFileExt(ChangeFileExt(FileName, ''));
     if SBeginsWith(ext2, '.') then Delete(ext2, 1, 1);
     if ext2<>'' then
       ext2:= ext2+'.'+ext1;
   end;
 
-  st:= TzStringList.Create;
-  try
-    //space separated
-    st.Delimiter:= ' ';
-
-    //find by double extension
-    if ext2<>'' then
-      for i:= 0 to LexLib.AnalyzerCount-1 do
-        with LexLib.Analyzers[i] do
-          if not Internal then
-          begin
-            st.DelimitedText:= Extentions;
-            if (st.IndexOf(ext2)>=0) then
-              Exit(LexLib.Analyzers[i]);
-          end;
-
-    //find by usual extension + filename
+  //find by double extension
+  if ext2<>'' then
     for i:= 0 to LexLib.AnalyzerCount-1 do
       with LexLib.Analyzers[i] do
         if not Internal then
-        begin
-          st.DelimitedText:= Extentions;
-          if ((ext1<>'') and (st.IndexOf(ext1)>=0)) or
-                            (st.IndexOf(fname)>=0) then
+          if SItemListed(ext2, Extentions) then
             Exit(LexLib.Analyzers[i]);
-        end;
-  finally
-    st.Free;
-  end;
+
+  //find by extension/filename
+  for i:= 0 to LexLib.AnalyzerCount-1 do
+    with LexLib.Analyzers[i] do
+      if not Internal then
+        if SItemListed(ext1, Extentions) or
+           SItemListed(fname, Extentions) then
+          Exit(LexLib.Analyzers[i]);
 end;
+
 
 function DoGetLexerFileFilter(an: TecSyntAnalyzer; const AllFilesText: string): string;
 var
